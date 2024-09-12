85f8c82ba6d9cd79407b3b263fc3ec6bd7555196
Anmol Bansal <anmolbansal1807@gmail.com>
2024-09-12 19:41:25 +0530

    start the c5 large

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -10,7 +10,7 @@ data "aws_ami" "amazon_linux_2023" {
 
 resource "aws_instance" "enclave_instance" {
   ami           = data.aws_ami.amazon_linux_2023.id
-  instance_type = "t3.medium"
+  instance_type = "c5.xlarge"
   subnet_id     = module.vpc.public_subnets[0]
 
   vpc_security_group_ids = [
