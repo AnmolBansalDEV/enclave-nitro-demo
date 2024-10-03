92a67e7b225b5cccba113b0b1b57a35f07399a1d
Anmol Bansal <anmolbansal1807@gmail.com>
2024-10-03 13:46:06 +0530

    access internet check

--- src/server/src/lib.rs
+++ src/server/src/lib.rs
@@ -4,10 +4,23 @@ use axum::{
 };
 use system::dmesg;
 
+async fn access_internet() -> String {
+    let url = "https://jsonplaceholder.typicode.com/todos/1";
+    let response = reqwest::get(url)
+    .await
+    .unwrap()
+    .text()
+    .await
+    .unwrap();
+    
+    response
+}
+
 pub async fn start_server() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
+    let app = Router::new()
+    .route("/", get(|| async { "Hello, World!" }))
+    .route("/access-internet", get(access_internet));
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
